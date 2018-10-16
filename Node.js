580c6741dad7494afaefbbea843aef93948902c7
Unknown <joaoalvaro.ferreira@hotmail.com>
2018-10-16 12:13:39 +0100

    Node

---
+++ Node.js
@@ -0,0 +1,31 @@
+//Node no = new Node(...);
+//console.log(no);
+
+class Node(){
+
+    constructor(obj,idNode){
+
+        this.graph = obj;
+        this.idNode = idNode;
+        this.material = null;
+        this.texture = null;
+        this.mattransf = null;
+        this.descendents = [];
+    };
+
+    insert(nodeName){
+        this.descendents.push(nodeName);
+    }
+
+    getSize(){
+        return this.descendents.length;
+    }
+
+    setMaterial(material){
+        this.material = material;
+    }
+
+    setMatrix(m){
+        this.mattransf = math.clone(m);
+    }
+}
