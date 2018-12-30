c94def8d14192a869d5d09312987453c98b8eb65
SPDHenry11 <esl-henrique-2010@hotmail.com>
2018-12-30 17:04:40 +0000

    visual base

--- Project 3/reader/library/MyCity.js
+++ Project 3/reader/library/MyCity.js
@@ -15,9 +15,9 @@
         this.texture = new CGFtexture(this.scene,"./scenes/images/" + playerTexture + ".jpg");
 
         this.myMaterial = new CGFappearance(this.scene);
-   		this.myMaterial.setAmbient(0.5, 0.5, 0.5, 1);
-		this.myMaterial.setDiffuse(0.7, 0.7, 0.7, 1);
-		this.myMaterial.setSpecular(0.7, 0.7, 0.7, 1);	
+   	  	this.myMaterial.setAmbient(0.5, 0.5, 0.5, 1);
+	    	this.myMaterial.setDiffuse(0.7, 0.7, 0.7, 1);
+		    this.myMaterial.setSpecular(0.7, 0.7, 0.7, 1);	
         this.myMaterial.setShininess(120);
         this.myMaterial.setTexture(this.texture);
   
@@ -26,26 +26,21 @@
       display() {
          this.myMaterial.apply();
          this.scene.pushMatrix();
-         this.scene.scale(1.5,2,1.5);  	
+         this.scene.scale(1.7,2,1.7);  
+         this.scene.rotate(90*DEGREE_TO_RAD,0,1,0);
+         this.scene.translate(0,-0.15,0);	
          //house1
          this.scene.pushMatrix();
-         /*<translate x="-0.1" y="0" z="0" />
-         <scale x="0.6" y="0.5" z="0.6" />
-         <rotate axis="y" angle="-20" /> */
          this.scene.translate(-0.1,0,0);
          this.scene.scale(0.6,0.5,0.6);
          this.scene.rotate(-20*DEGREE_TO_RAD,0,1,0);
          //base
-         // <translate x="0" y="0.25" z="0" />
-         // <scale x="0.4" y="0.5" z="0.4" />
          this.scene.pushMatrix();      
          this.scene.translate(0,0.25,0);
          this.scene.scale(0.4,0.5,0.4);
          this.cube.display();
-          this.scene.popMatrix();
+         this.scene.popMatrix();
          //roof
-         //<translate x="0" y="0.4" z="0" />
-         //<scale x="0.6" y="0.4" z="0.6" />
          this.scene.pushMatrix();      
          this.scene.translate(0,0.4,0);
          this.scene.scale(0.6,0.4,0.6);
@@ -56,23 +51,16 @@
   
            //house2
          this.scene.pushMatrix();
-         /*<translate x="0.2" y="0" z="0" />
-         <scale x="0.4" y="0.3" z="0.4" />
-         <rotate axis="y" angle="20" />*/
          this.scene.translate(0.2,0,0);
          this.scene.scale(0.4,0.3,0.4);
          this.scene.rotate(20*DEGREE_TO_RAD,0,1,0);
          //base
-         // <translate x="0" y="0.25" z="0" />
-         // <scale x="0.4" y="0.5" z="0.4" />
          this.scene.pushMatrix();      
          this.scene.translate(0,0.25,0);
          this.scene.scale(0.4,0.5,0.4);
          this.cube.display();
          this.scene.popMatrix();
          //roof
-         //<translate x="0" y="0.4" z="0" />
-         //<scale x="0.6" y="0.4" z="0.6" />
          this.scene.pushMatrix();      
          this.scene.translate(0,0.4,0);
          this.scene.scale(0.6,0.4,0.6);
